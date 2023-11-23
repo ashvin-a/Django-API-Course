2174f3e8c3ce7fb6fe3faa58049b033e40565d65
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2023-11-23 22:54:17 +0530

    added sample code to check testing

---
+++ app/app/calcu.py
@@ -0,0 +1,12 @@
+'''
+Calculator functions
+'''
+
+
+def add(x,y):
+    '''Add two numbers'''
+    return x+y
+
+def subtract(x,y):
+    '''SUbtracts two numbers'''
+    return x-y
---
+++ app/app/test.py
@@ -0,0 +1,19 @@
+'''
+Sample test
+'''
+
+from django.test import SimpleTestCase
+from app import calcu
+
+class CalcTest(SimpleTestCase):
+    '''Test the calc module'''
+
+    def test_add(self):
+        res = calcu.add(1,2)
+
+        self.assertEqual(res,3)
+
+    def test_sub(self):
+        res = calcu.subtract(2,1)
+
+        self.assertEqual(res,1)
