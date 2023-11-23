df4227e2fae9ef5695e4b63a7c8d3eba78444461
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2023-11-23 22:59:09 +0530

    linting fixes

--- app/app/calcu.py
+++ app/app/calcu.py
@@ -3,10 +3,10 @@ Calculator functions
 '''
 
 
-def add(x,y):
+def add(x, y):
     '''Add two numbers'''
     return x+y
 
-def subtract(x,y):
+def subtract(x, y):
     '''SUbtracts two numbers'''
     return x-y
--- app/app/test.py
+++ app/app/test.py
@@ -8,12 +8,13 @@ from app import calcu
 class CalcTest(SimpleTestCase):
     '''Test the calc module'''
 
-    def test_add(self):
-        res = calcu.add(1,2)
 
-        self.assertEqual(res,3)
+    def test_add(self) :
+        res = calcu.add(1, 2)
+
+        self.assertEqual(res, 3)
 
     def test_sub(self):
-        res = calcu.subtract(2,1)
+        res = calcu.subtract(2, 1)
 
-        self.assertEqual(res,1)
+        self.assertEqual(res, 1)
