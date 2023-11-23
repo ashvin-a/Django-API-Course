f5e34a7994d3bf6d722e9831fd4b480db178b373
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2023-11-23 23:02:29 +0530

    linting is pain

--- app/app/calcu.py
+++ app/app/calcu.py
@@ -7,6 +7,7 @@ def add(x, y):
     '''Add two numbers'''
     return x+y
 
+
 def subtract(x, y):
     '''SUbtracts two numbers'''
     return x-y
--- app/app/test.py
+++ app/app/test.py
@@ -5,11 +5,11 @@ Sample test
 from django.test import SimpleTestCase
 from app import calcu
 
+
 class CalcTest(SimpleTestCase):
     '''Test the calc module'''
 
-
-    def test_add(self) :
+    def test_add(self):
         res = calcu.add(1, 2)
 
         self.assertEqual(res, 3)
@@ -18,3 +18,4 @@ class CalcTest(SimpleTestCase):
         res = calcu.subtract(2, 1)
 
         self.assertEqual(res, 1)
+        
